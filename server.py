d5a7dcf0d9bfa1cb4d13a4007b566e9f7890162e
Conor Freeland <conor.booms.freeland@gmail.com>
2017-03-16 12:37:06 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,60 @@
+import os
+import glob
+import time
+import json
+
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from Adafruit_PWM_Servo_Driver import PWM
+
+app = Flask(__name__)
+CORS(app)
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+	f = open(device_file, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
+
+def read_temp():
+	lines = read_temp_raw()
+	equals_pos = lines[1].find('t=')
+	if equals_pos != -1:
+		temp_str = lines[1][equals_pos+2:]
+		temp_c = float(temp_str) / 1000.0
+		temp_f = temp_c * 9.0 / 5.0 + 32.0
+		temps = {}
+		temps["c"] = temp_c
+		temps["f"] = temp_f
+		temps_json = json.dumps(temps)
+		return temps_json
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+@app.route("/feed/")
+def feed():
+	pwm = PWM(0x40)
+	pwm.setPWMFreq(50)
+
+	pwm.setPWM(0, 0, 550)
+	time.sleep(2)
+
+	for x in range(0, 4):
+		pwm.setPWM(0, 0, 128)
+		time.sleep(0.5)
+		pwm.setPWM(0, 0, 180)
+		time.sleep(0.5)
+	return "Feeding complete"
+
+@app.route("/temp/")
+def temp():
+	return read_temp()
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
